fc6b53ccc0428009f26e65a4ce1850c75037d4f0
Liyuanzhe2001 <619356147@qq.com>
2023-05-11 01:03:22 +0800

    update

--- com/lyz/code/main_1016.java
+++ com/lyz/code/main_1016.java
@@ -9,7 +9,6 @@ public class main_1016 {
     }
 
     public boolean queryString(String s, int n) {
-        n = Math.min(Integer.parseInt(s, 2), n);
         for (int i = 1; i <= n; i++) {
             if(!s.contains(Integer.toBinaryString(i))){
                 return false;
