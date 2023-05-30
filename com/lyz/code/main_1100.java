03cc9c1135205baede7fd9d54e6ffa44d754a807
Liyuanzhe2001 <619356147@qq.com>
2023-05-30 09:18:20 +0800

    update

---
+++ com/lyz/code/main_1100.java
@@ -0,0 +1,49 @@
+package com.lyz.code;
+
+import com.lyz.code.dataStructure.TreeNode;
+
+import java.awt.event.WindowFocusListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class main_1100 {
+
+    public static void main(String[] args) {
+
+    }
+
+    List<TreeNode> res = new ArrayList<>();
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        List<Integer> list = new ArrayList<>() {{
+            for (int i : to_delete) {
+                add(i);
+            }
+        }};
+        if (!list.contains(root.val)) {
+            res.add(root);
+        }
+        dfs(root, false, list);
+        return res;
+    }
+
+    public TreeNode dfs(TreeNode node, boolean isAdd, List<Integer> list) {
+        if (node == null) {
+            return null;
+        }
+        node.left = dfs(node.left, list.contains(node.val), list);
+        node.right = dfs(node.right, list.contains(node.val), list);
+        if (list.contains(node.val)) {
+            return null;
+        } else {
+            if (isAdd) {
+                res.add(node);
+            }
+        }
+        return node;
+    }
+
+
+}
