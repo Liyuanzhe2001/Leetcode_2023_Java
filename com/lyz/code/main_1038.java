9ac06ab63f1cb267510e52a367acad92d516b19c
Liyuanzhe2001 <619356147@qq.com>
2023-06-01 15:35:26 +0800

    update

--- com/lyz/code/main_1038.java
+++ com/lyz/code/main_1038.java
@@ -1,2 +1,28 @@
-package com.lyz.code;public class main_1038 {
+package com.lyz.code;
+
+import com.lyz.code.dataStructure.TreeNode;
+
+public class main_1038 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode bstToGst(TreeNode root) {
+        dfs(root);
+        return root;
+    }
+
+    TreeNode pre = new TreeNode(0);
+
+    public void dfs(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        dfs(node.right);
+        node.val = node.val + pre.val;
+        pre = node;
+        dfs(node.left);
+    }
+
 }
--- com/lyz/code/main_2517.java
+++ com/lyz/code/main_2517.java
@@ -1,2 +1,41 @@
-package com.lyz.code;public class main_2517 {
+package com.lyz.code;
+
+import java.util.Arrays;
+
+public class main_2517 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int maximumTastiness(int[] price, int k) {
+        Arrays.sort(price);
+        int lef = 0;
+        int rig = price[price.length - 1] - price[0];
+        int res = 0;
+        while (lef <= rig) {
+            int mid = (lef + rig - 1) >> 1;
+            if (cal(price, k, mid)) {
+                lef = mid + 1;
+                res = mid;
+            } else {
+                rig = mid - 1;
+            }
+        }
+        return res;
+    }
+
+    public boolean cal(int[] price, int k, int mid) {
+        int pre = price[0];
+        int cnt = 1;
+        for (int i = 1; i < price.length; i++) {
+            if (price[i] - pre >= mid) {
+                cnt++;
+                pre = price[i];
+            }
+        }
+        return cnt >= k;
+    }
+
+
 }
