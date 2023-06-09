939c911a379ae76a33fba37f223e5bde6465c316
Liyuanzhe2001 <619356147@qq.com>
2023-06-09 10:22:08 +0800

    update

--- com/lyz/code/main_1367.java
+++ com/lyz/code/main_1367.java
@@ -1,2 +1,42 @@
-package com.lyz.code;public class main_1367 {
+package com.lyz.code;
+
+import com.lyz.code.dataStructure.ListNode;
+import com.lyz.code.dataStructure.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class main_1367 {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode treeNode = queue.poll();
+            if (treeNode.val == head.val) {
+                if (dfs(head, treeNode)) {
+                    return true;
+                }
+            }
+            if (treeNode.left != null) {
+                queue.add(treeNode.left);
+            }
+            if (treeNode.right != null) {
+                queue.add(treeNode.right);
+            }
+        }
+        return false;
+    }
+
+    public boolean dfs(ListNode node, TreeNode root) {
+        if (node == null) {
+            return true;
+        }
+        if (root == null) {
+            return false;
+        }
+        if (node.val != root.val) {
+            return false;
+        }
+        return dfs(node.next, root.left) || dfs(node.next, root.right);
+    }
 }
--- com/lyz/code/main_430.java
+++ com/lyz/code/main_430.java
@@ -1,2 +1,42 @@
-package com.lyz.code;public class main_430 {
+package com.lyz.code;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class main_430 {
+
+    class Node {
+        public int val;
+        public Node prev;
+        public Node next;
+        public Node child;
+    }
+
+    public Node flatten(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Node root;
+        root = head;
+        while (head != null) {
+            if (head.child == null) {
+                head = head.next;
+            } else {
+                Node tmp = head.next;
+                Node chileNode = flatten(head.child);
+                head.next = chileNode;
+                chileNode.prev = head;
+                head.child = null;
+                while (head.next != null) {
+                    head = head.next;
+                }
+                head.next = tmp;
+                if (tmp != null) {
+                    tmp.prev = head;
+                }
+            }
+        }
+        return root;
+    }
+
 }
