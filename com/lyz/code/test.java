08317a59644816a413d392ebd8515570497c270e
Liyuanzhe2001 <619356147@qq.com>
2023-08-14 23:54:20 +0800

    update

--- com/lyz/code/test.java
+++ com/lyz/code/test.java
@@ -4,12 +4,6 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        System.out.println(list.subList(0, 3));
-        System.out.println(list.subList(3, list.size()));
+        System.out.println(Math.abs((long) -2147483648));
     }
 }
---
+++ com/lyz/code/数据结构教程/第一章/lc_整数翻转.java
@@ -0,0 +1,25 @@
+package com.lyz.code.数据结构教程.第一章;
+
+/**
+ * @author lkunk
+ * @date 2023年08月14日 23:39
+ * @description
+ */
+
+public class lc_整数翻转 {
+    public int reverse(int x) {
+        String res = "";
+        boolean flag = x < 0;
+        long tmp = Math.abs((long) x);
+        if (tmp / 10 == 0) {
+            return x;
+        }
+        while (tmp != 0) {
+            res = res + tmp % 10;
+            tmp /= 10;
+        }
+        int MAX_VAL = (1 << 31) - 1, MIN_VAL = -(1 << 31);
+        long i = flag ? -Long.parseLong(res) : Long.parseLong(res);
+        return i >= MAX_VAL || i < MIN_VAL ? 0 : (int) i;
+    }
+}
