da640fbe3d020ef07a344a702d58fc3aedf706ec
Liyuanzhe2001 <619356147@qq.com>
2023-08-23 23:24:44 +0800

    update

--- com/lyz/code/main_1782.java
+++ com/lyz/code/main_1782.java
@@ -1,66 +1,66 @@
-package com.lyz.code;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * @author lkunk
- * @date 2023年08月23日 23:23
- * @description
- */
-
-public class main_1782 {
-    class Solution {
-        public int[] countPairs(int n, int[][] edges, int[] queries) {
-            int[] degree = new int[n];
-            Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
-            for (int[] edge : edges) {
-                int x = edge[0] - 1, y = edge[1] - 1;
-                if (x > y) {
-                    int temp = x;
-                    x = y;
-                    y = temp;
-                }
-                degree[x]++;
-                degree[y]++;
-                cnt.put(x * n + y, cnt.getOrDefault(x * n + y, 0) + 1);
-            }
-
-            int[] arr = Arrays.copyOf(degree, n);
-            int[] ans = new int[queries.length];
-            Arrays.sort(arr);
-            for (int k = 0; k < queries.length; k++) {
-                int bound = queries[k], total = 0;
-                for (int i = 0; i < n; i++) {
-                    int j = binarySearch(arr, i + 1, n - 1, bound - arr[i]);
-                    total += n - j;
-                }
-                for (Map.Entry<Integer, Integer> entry : cnt.entrySet()) {
-                    int val = entry.getKey(), freq = entry.getValue();
-                    int x = val / n, y = val % n;
-                    if (degree[x] + degree[y] > bound && degree[x] + degree[y] - freq <= bound) {
-                        total--;
-                    }
-                }
-                ans[k] = total;
-            }
-
-            return ans;
-        }
-
-        public int binarySearch(int[] arr, int left, int right, int target) {
-            int ans = right + 1;
-            while (left <= right) {
-                int mid = (left + right) >> 1;
-                if (arr[mid] <= target) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                    ans = mid;
-                }
-            }
-            return ans;
-        }
-    }
-}
+//package com.lyz.code;
+//
+//import java.util.Arrays;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+///**
+// * @author lkunk
+// * @date 2023年08月23日 23:23
+// * @description
+// */
+//
+//public class main_1782 {
+//    class Solution {
+//        public int[] countPairs(int n, int[][] edges, int[] queries) {
+//            int[] degree = new int[n];
+//            Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
+//            for (int[] edge : edges) {
+//                int x = edge[0] - 1, y = edge[1] - 1;
+//                if (x > y) {
+//                    int temp = x;
+//                    x = y;
+//                    y = temp;
+//                }
+//                degree[x]++;
+//                degree[y]++;
+//                cnt.put(x * n + y, cnt.getOrDefault(x * n + y, 0) + 1);
+//            }
+//
+//            int[] arr = Arrays.copyOf(degree, n);
+//            int[] ans = new int[queries.length];
+//            Arrays.sort(arr);
+//            for (int k = 0; k < queries.length; k++) {
+//                int bound = queries[k], total = 0;
+//                for (int i = 0; i < n; i++) {
+//                    int j = binarySearch(arr, i + 1, n - 1, bound - arr[i]);
+//                    total += n - j;
+//                }
+//                for (Map.Entry<Integer, Integer> entry : cnt.entrySet()) {
+//                    int val = entry.getKey(), freq = entry.getValue();
+//                    int x = val / n, y = val % n;
+//                    if (degree[x] + degree[y] > bound && degree[x] + degree[y] - freq <= bound) {
+//                        total--;
+//                    }
+//                }
+//                ans[k] = total;
+//            }
+//
+//            return ans;
+//        }
+//
+//        public int binarySearch(int[] arr, int left, int right, int target) {
+//            int ans = right + 1;
+//            while (left <= right) {
+//                int mid = (left + right) >> 1;
+//                if (arr[mid] <= target) {
+//                    left = mid + 1;
+//                } else {
+//                    right = mid - 1;
+//                    ans = mid;
+//                }
+//            }
+//            return ans;
+//        }
+//    }
+//}
